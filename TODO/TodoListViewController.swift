c3030088f32eca0c115a3659a2f5ff1ea185b299
zpcao <shenlie_@126.com>
2019-01-03 00:07:13 +0800

    test

---
+++ TODO/TodoListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by zpcao on 2019/1/2.
+//  Copyright © 2019 zpcao.com. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    var itemArray = ["购买水杯","吃药","修改密码"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView:UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text = itemArray[indexPath.row]
+        return cell
+    }
+
+    override func tableView(_ tableView:UITableView, numberOfRowsInSection section:Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView:UITableView, didSelectRowAt indexPath:IndexPath) {
+        print(indexPath.row)
+        print(itemArray[indexPath.row])
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+         
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "添加一个新的ToDo项目", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "添加项目", style: .default) {
+            (action) in
+            print(textField.text!)
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        alert.addTextField{
+            (alertTextField) in
+            alertTextField.placeholder = "创建一个新项目..."
+//            print(alertTextField.text!)
+            textField = alertTextField
+        }
+        
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+        
+    }
+}
+
