05c2e740afce4f04625b0a597647a102052c3493
zpcao <shenlie_@126.com>
2019-01-03 21:54:42 +0800

    codable

---
+++ TODO/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  TODO
+//
+//  Created by zpcao on 2019/1/3.
+//  Copyright © 2019 zpcao.com. All rights reserved.
+//
+
+import Foundation
+
+class Item : Codable{
+    var title = ""
+    var done = false
+}
--- TODO/TodoListViewController.swift
+++ TODO/TodoListViewController.swift
@@ -10,22 +10,56 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    var itemArray = ["购买水杯","吃药","修改密码"]
+//    var itemArray = ["购买水杯","吃药","修改密码"]
+    var itemArray = [Item]()
     
-    let defaults = UserDefaults.standard
+    
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+
+    
+//    let defaults = UserDefaults.standard
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            itemArray = items
-        }
+//        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
+//            itemArray = items
+//        }
+        print(dataFilePath)
+        loadItems()
+        
+//        let newItem = Item()
+//        newItem.title = "购买水杯"
+//        itemArray.append(newItem)
+//
+//        let newItem2 = Item()
+//        newItem2.title = "吃药"
+//        itemArray.append(newItem2)
+//
+//
+//        let newItem3 = Item()
+//        newItem3.title = "修改密码"
+//        itemArray.append(newItem3)
+        
+        
+
+
         // Do any additional setup after loading the view, typically from a nib.
     }
     
     override func tableView(_ tableView:UITableView, cellForRowAt indexPath:IndexPath) -> UITableViewCell{
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-        cell.textLabel?.text = itemArray[indexPath.row]
+        cell.textLabel?.text = itemArray[indexPath.row].title
+        
+//        if itemArray[indexPath.row].done == false {
+//            cell.accessoryType = .none
+//        } else {
+//            cell.accessoryType = .checkmark
+//        }
+        
+        let item = itemArray[indexPath.row]
+        cell.accessoryType = item.done == true ? .checkmark : .none
+        
         return cell
     }
 
@@ -45,7 +79,41 @@ class TodoListViewController: UITableViewController {
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
         }
         
+        if itemArray[indexPath.row].done == false {
+            itemArray[indexPath.row].done = true
+        } else {
+            itemArray[indexPath.row].done = false
+        }
+        
+        saveItems()
+        
+        tableView.beginUpdates()
+        tableView.reloadRows(at: [indexPath], with: UITableView.RowAnimation.none)
+        tableView.endUpdates()
+        
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func saveItems(){
+        let encoder = PropertyListEncoder()
+        do {
+            let data = try encoder.encode(itemArray)
+            try data.write(to: dataFilePath!)
+        } catch {
+            print("编码错误:\(error)")
+        }
+    }
+    
+    func loadItems(){
+        if let data = try?Data(contentsOf:dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+                itemArray = try decoder.decode([Item].self, from: data)
+                
+            } catch {
+                print("解码item错误！")
+            }
+        }
     }
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -55,8 +123,13 @@ class TodoListViewController: UITableViewController {
         let action = UIAlertAction(title: "添加项目", style: .default) {
             (action) in
             print(textField.text!)
-            self.itemArray.append(textField.text!)
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
+            let newItem = Item()
+            newItem.title = textField.text!
+            self.itemArray.append(newItem)
+
+            self.saveItems()
+
+            //            self.defaults.set(self.itemArray, forKey: "ToDoListArray")
             self.tableView.reloadData()
         }
         
